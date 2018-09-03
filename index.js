7a691ae6a0784d4852f0cf98be7fa126c73d8d0c
Alex <lex.2418@gmail.com>
2018-09-03 10:55:15 -0500

    Add presets

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+  return {
+    plugins: [
+      require("@babel/plugin-proposal-function-bind"),
+      require("@babel/plugin-proposal-throw-expressions"),
+      require("@babel/plugin-proposal-logical-assignment-operators"),
+      require("@babel/plugin-proposal-export-default-from"),
+      [require("@babel/plugin-proposal-optional-chaining"), { "loose": false }],
+      [require("@babel/plugin-proposal-pipeline-operator"), { "proposal": "minimal" }],
+      [require("@babel/plugin-proposal-nullish-coalescing-operator"), { "loose": false }],
+      require("@babel/plugin-proposal-do-expressions"),
+    ],
+
+    presets: [
+      "@babel/preset-env"
+    ],
+  }
+}
